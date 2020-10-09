cae06989f6165fde78dd5070330d1b48ea07aebf
aleksandrshtebe <72428554+aleksandrshtebe@users.noreply.github.com>
2020-10-09 20:13:06 +0900

    Add files via upload

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Class1
+    {
+        double vozrast;
+        double rost;
+        string Name;
+        string status;
+
+        public Class1(double vozrast, double rost, string name, string status)
+        {
+            this.vozrast = vozrast;
+            this.rost = rost;
+            Name = name;
+            this.status = status;
+        }
+        public double ReturnVozrast()
+        {
+            return vozrast;
+        }
+        public string ReturnName()
+        {
+            return Name;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public double ReturnRost()
+        {
+            return rost;
+        }
+        public string ReturnStatus()
+        {
+            return status;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 studetn = new Class1 (20, 185, "Александр", "Обучается");
+            Console.WriteLine($"Возраст студента {studetn.ReturnVozrast()} ");
+            Console.WriteLine($"Рост студента {studetn.ReturnRost()} ");
+            Console.WriteLine($"Имя студента {studetn.ReturnName()} ");
+            Console.WriteLine($"Статус Студента {studetn.ReturnStatus()} ");
+                Console.WriteLine("Введите новое имя студента _");
+            studetn.SetName(Console.ReadLine());
+            Console.WriteLine($"Новое имя студента {studetn.ReturnName()}");
+            Console.ReadLine();
+        }
+    }
+}
